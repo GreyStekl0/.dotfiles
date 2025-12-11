1b4f5b651fe230d690ff200aa92d1fe2f47ef0f7
Sergey Gulyev <greystekl0@gmail.com>
2025-12-11 09:02:05 +0500

    add abbr for sudo (doas) and man (tldr)

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -2,3 +2,5 @@ abbr -a up topgrade
 abbr -a --position anywhere -- --help '--help | bat -plhelp'
 abbr -a --position anywhere -- -h '-h | bat -plhelp'
 abbr -a ls 'eza --icons'
+abbr -a sudo doas
+abbr -a man tldr
