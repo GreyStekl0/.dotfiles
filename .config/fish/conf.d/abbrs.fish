b52a9e13a090bfdb93afff105ee9f2c06908f893
Maxim Dubrovsky <maxdubmors@gmail.com>
2026-04-13 17:12:58 +0500

    add niri

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -1,9 +1,5 @@
 abbr -a up topgrade
-abbr -a cat bat
-abbr -a --position anywhere -- --help '--help | bat -plhelp'
-abbr -a --position anywhere -- -h '-h | bat -plhelp'
 abbr -a sudo doas
 abbr -a lg lazygit
-abbr -a npx bunx
-abbr -a pipx uvx
-abbr -a brew zb
+abbr -a --position anywhere -- --help '--help | bat -plhelp'
+abbr -a --position anywhere -- -h '-h | bat -plhelp'
